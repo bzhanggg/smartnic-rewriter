2e53de677916b63cc11fbdb5527059fcd7c2db1b
bzhanggg <blz19@scarletmail.rutgers.edu>
2024-11-10 17:14:24 -0500

    finish docker setup

--- install.sh
+++ install.sh
@@ -1,35 +1,7 @@
 #!/bin/bash
-# download clang-llvm
-# wget https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
-# tar -xf clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
-# rm clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
-
-# install necessary libtooling tools:
-sudo apt-get update
-sudo apt-get install libtinfo-dev
-sudo apt-get install zlib1g-dev
-sudo apt-get install libelf-dev
-sudo apt-get install clang llvm
-
-sudo apt-get update && apt-get install -y \
-    wget \
-    git \
-    openssh-client \
-    build-essential \
-    libtinfo-dev \
-    zlib1g-dev \
-    libelf-dev \
-    clang \
-    llvm \
-    && rm -rf /var/lib/apt/lists/*
-
-# install linux source code
-# wget https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/snapshot/linux-5.4.tar.gz
-# tar -xvf linux-5.4.tar.gz
-# rm -rf linux-5.4.tar.gz
 
 # install, and prepare the correct linux kernel source code 
-cd /usr/local/linux-5.4
+cd /linux-5.4
 make -C tools clean
 make -C samples/bpf clean
 make clean
